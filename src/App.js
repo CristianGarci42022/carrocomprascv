62e746cac839f895022963d956d3e66e2de16f4f
Cristian Garcia <107603821+CristianGarci42022@users.noreply.github.com>
2023-03-20 21:32:33 -0500

    Se agrego el componente para el carro de compras

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 
-// import Producto from './componentes/Producto';
 import './App.css';
 import Navbar from './componentes/Navbar';
-import Productos from './componentes/Productos';
+// import Productos from './componentes/Productos';
+// import Producto from './componentes/Producto';
+// import TarjetaPago from "./componentes/TarjetaPago";
+import PaginaPago from "./componentes/PaginaPago";
+
+
+
 
 
 function App() {
   return (
     <div className="App">
-       {/*<Producto/>*/}
         <Navbar/>
-    
-        <Productos/>
+        <PaginaPago/>
+        {/*<TarjetaPago/>*/}
+        {/*<Producto/>*/}
+        {/*<Productos/>*/}
          
     </div>
   );
